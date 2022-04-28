57dfa7e1a6701bc7856486cc384bb75a2493d7e5
Marius <marius@example.com>
2022-04-28 14:06:25 +0300

    tarpine

--- readme.md
+++ readme.md
@@ -8,3 +8,12 @@
 3. atvaizduojam cart masyva po prekiu sarasu.
 4. Pagaminam lentele su title(primi trys title zodziai), price(kaina), category(kategorija), image(paveiklselis)
 5. pridedam select lauka virs lenteles/saraso kuriame yra visos kategorijos is items. Parinkus kategorija rodom tik tos kategorijos items
+
+6. <button id="sort-price">Sort By Price</button> nusitaikyti ir iskviesti sortByPrice() kuri iskonsolina "sort"
+
+7. sortByPrice() fn viduje isrikiuoti items masyva ir paduoti ji i makelShopList
+
+## tarpine uzduotis
+
+1t. Paspaudus mygtuka 'minus' mazinam counter reiksme.
+2t. Neleisti mazinti maziau nulio.
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 // 1d. Sugeneruoti items ul elemente li elementus su prekiu pavadinimu ir kaina ir mygtuku buy
 
-import { ItemObj, items } from './data/db';
+import { ItemObj, items } from './data/db.js';
 
 const listEl = document.getElementById('shop-list') as HTMLOListElement | null;
 
@@ -8,7 +8,7 @@ const listEl = document.getElementById('shop-list') as HTMLOListElement | null;
 function makeLiEl(title: string, price: number): HTMLLIElement {
   const li = document.createElement('li');
   const strongEl = document.createElement('strong');
-  strongEl.textContent = title;
+  strongEl.textContent = title.slice(0, 12);
   li.appendChild(strongEl);
 
   li.append(` - ${price} `);
@@ -21,13 +21,15 @@ function makeLiEl(title: string, price: number): HTMLLIElement {
 }
 // idedam viena item 'shoes' 200
 // makeLiEl('shoes', 200);
-const liShoues = makeLiEl('shoes', 200);
-listEl?.appendChild(liShoues);
+// const liShoues = makeLiEl('shoes', 200);
+// listEl?.appendChild(liShoues);
 
 function makelShopList(arr: ItemObj[]): void {
   arr.forEach((arrItem: ItemObj): void => {
     // create item su makeLiEl
+    const liEl: HTMLLIElement = makeLiEl(arrItem.title, arrItem.price);
     // append item created
+    listEl?.appendChild(liEl);
   });
 }
 makelShopList(items);
